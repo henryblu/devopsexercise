9888242beb92baecc556795251d394b7deb2367d
henryblu <henry.s.blue@gmail.com>
2023-06-16 16:08:20 +0300

    test to see if the pipeline is set up right

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-  res.send('<h1>test!</h1>')
+  res.send('<h1>Hello this World!</h1>')
 })
 
 const PORT = 8080
